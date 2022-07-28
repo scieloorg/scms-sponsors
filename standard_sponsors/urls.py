d6cbc07feba9e9a347c36fbbda482943f38694af
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2022-07-27 21:28:04 -0300

    Renomeia app standard_sponsors

--- standard_sponsors/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import import_file, validate
-
-app_name = "standard_sponsors"
-urlpatterns = [
-    path("validate", view=validate, name="validate"),
-    path("import", view=import_file, name="import_file"),
-]
